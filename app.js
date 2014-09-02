a74f1e8da9c2275b329623ca06505394a4c33bd7
Andrew Li <andrewli@dimsumlab.com>
2014-09-02 13:24:00 +0800

    Minor formatting fixes

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // Module dependencies
- 
 var express = require('express');
 var routes = require('./routes');
-    
+
 var app = module.exports = express();
 
 // Environment setup
@@ -24,7 +23,7 @@ app.configure('production', function(){
 
 // Routes
 app.get('/', routes.index);
- 
-// Begin listening
+
+// Start server
 app.listen(app.get('port'));
 console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
