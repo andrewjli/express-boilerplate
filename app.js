5985f8f6eeb20ac46250696d6e9622050efe73dc
Andrew Li <andrewli@dimsumlab.com>
2014-09-02 12:59:17 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// Module dependencies
+ 
+var express = require('express');
+var routes = require('./routes');
+    
+var app = module.exports = express();
+
+// Environment setup
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.logger('dev'))
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/static'))
+//app.use(favicon(__dirname + '/public/images/favicon.png'));
+app.configure('development', function(){
+    app.use(express.errorHandler());
+    app.locals.pretty = true;
+});
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+app.get('/', routes.index);
+ 
+// Begin listening
+app.listen(app.get('port'));
+console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
